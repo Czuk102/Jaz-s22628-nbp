bd51694dba4991d2bcda9bb464345ed1cffb6a10
Emilian Murawski <murawski.emilian@gmail.com>
2022-06-18 09:34:08 +0200

    Initial commit

---
+++ src/main/java/com/example/jazs22628nbp/services/NbpService.java
@@ -0,0 +1,14 @@
+package com.example.jazs22628nbp.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.Root;
+
+@Service
+public class NbpService {
+    
+
+    public Root getCurrency(String code, Integer topCount) {
+        return null;
+    }
+}
